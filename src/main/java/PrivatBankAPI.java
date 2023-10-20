eb36997fd74d764d1bef1dfafaa456fc04a5f1eb
Zahar Fedorchuk <puzeblin888@gmail.com>
2023-10-20 15:32:50 +0300

    ADD Privat

--- src/main/java/PrivatBankAPI.java
+++ src/main/java/PrivatBankAPI.java
@@ -1,24 +1,22 @@
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import com.google.gson.JsonElement;
-import java.util.Currency;
-import java.util.HashMap;
 
-public class MonobankAPI {
+public class PrivatBankAPI {
     private final String TOKEN_MONO = "u9q4pmlANZUadu_qsMwtJwwihPm1m22dDlXrBDQ8eXMU";
-    public String getExchangeRateMono(String currencyNameA, String currencyNameB) {
+    public String getExchangeRatePrivat(String currencyNameA, String currencyNameB) {
         double rateBuy = 0.0;
         double rateSell = 0.0;
         try {
-            URL url = new URL("https://api.monobank.ua/bank/currency");
+            URL url = new URL("https://api.privatbank.ua/p24api/pubinfo?exchange&coursid=5");
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
-            connection.setRequestProperty("X-Token", TOKEN_MONO);
             int respCode = connection.getResponseCode();
             if (respCode == HttpURLConnection.HTTP_OK) {
                 BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
@@ -32,14 +30,14 @@ public class MonobankAPI {
                 JsonArray currencyData = JsonParser.parseString(respBody).getAsJsonArray();
                 for (JsonElement element : currencyData) {
                     JsonObject currency = element.getAsJsonObject();
-                    int currencyCodeA = currency.get("currencyCodeA").getAsInt();
-                    int currencyCodeB = currency.get("currencyCodeB").getAsInt();
-                    if (currencyCodeA == Integer.parseInt(currencyNameA) && currencyCodeB == Integer.parseInt(currencyNameB)) {
-                        rateBuy = currency.get("rateBuy").getAsDouble();
-                        rateSell = currency.get("rateSell").getAsDouble();
+                    String currencyCodeA = currency.get("ccy").getAsString();
+                    String currencyCodeB = currency.get("base_ccy").getAsString();
+                    if (currencyCodeA.equals(currencyNameA) && currencyCodeB.equals(currencyNameB)) {
+                        rateBuy = currency.get("buy").getAsDouble();
+                        rateSell = currency.get("sale").getAsDouble();
                     }
                 }
-                return "Курс в MONOBANK:" + currencyNameA + "/" + currencyNameB + "\nКупівля: " + rateBuy + "\nПродаж: " + rateSell;
+                return "Курс в PrivatBank:" + currencyNameA + "/" + currencyNameB + "\nКупівля: " + rateBuy + "\nПродаж: " + rateSell;
             } else {
                 System.out.println("Error: " + respCode);
             }
--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,7 +3,8 @@ import java.util.Currency;
 public class Test {
     public static void main(String[] args) {
         MonobankAPI monobankAPI = new MonobankAPI();
+        PrivatBankAPI privatBankAPI = new PrivatBankAPI();
         System.out.println(monobankAPI.getExchangeRateMono("840", "980"));
-
+        System.out.println(privatBankAPI.getExchangeRatePrivat("USD", "UAH"));
     }
 }
