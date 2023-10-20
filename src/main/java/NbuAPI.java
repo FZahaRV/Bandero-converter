0d522862afcf9edeecba0cc0ca224a1bb25ee748
Zahar Fedorchuk <puzeblin888@gmail.com>
2023-10-20 16:10:49 +0300

    ADD NBU

---
+++ src/main/java/NbuAPI.java
@@ -0,0 +1,47 @@
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class NbuAPI {
+    private final String TOKEN_MONO = "u9q4pmlANZUadu_qsMwtJwwihPm1m22dDlXrBDQ8eXMU";
+    public String getExchangeRateNbu(String currencyNameA, String currencyNameB) {
+        double rateBuy = 0.0;
+        double rateSell = 0.0;
+        try {
+            URL url = new URL("https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            int respCode = connection.getResponseCode();
+            if (respCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                StringBuilder resp = new StringBuilder();
+                String input;
+                while ((input = in.readLine()) != null) {
+                    resp.append(input);
+                }
+                in.close();
+                String respBody = resp.toString();
+                JsonArray currencyData = JsonParser.parseString(respBody).getAsJsonArray();
+                for (JsonElement element : currencyData) {
+                    JsonObject currency = element.getAsJsonObject();
+                    int currencyCodeA = currency.get("r030").getAsInt();
+                    if (currencyCodeA == Integer.parseInt(currencyNameA)) {
+                        rateBuy = currency.get("rate").getAsDouble();
+                    }
+                }
+                return "Курс Національного банку України:" + currencyNameA + "/" + currencyNameB + "\nНБУ: " + rateBuy;
+            } else {
+                System.out.println("Error: " + respCode);
+            }
+        }  catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "Error";
+    }
+}
--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -4,7 +4,9 @@ public class Test {
     public static void main(String[] args) {
         MonobankAPI monobankAPI = new MonobankAPI();
         PrivatBankAPI privatBankAPI = new PrivatBankAPI();
+        NbuAPI nbuAPI = new NbuAPI();
         System.out.println(monobankAPI.getExchangeRateMono("840", "980"));
         System.out.println(privatBankAPI.getExchangeRatePrivat("USD", "UAH"));
+        System.out.println(nbuAPI.getExchangeRateNbu("840", "980"));
     }
 }
